161058ca6907228c083f761a8911e4536d0b07db
HeJason <hejason@HeJasondeMacBook-Pro.local>
2017-08-15 20:05:46 +0800

    test haha controler

--- src/Demo/app.js
+++ src/Demo/app.js
@@ -1,6 +1,6 @@
  angular.module('test', ['ng-keybord-select'])
      .controller('TestCtrl', function($scope, $filter, $log) {
-         console.log("test Controller");
+         console.log("test haha Controller");
          var vm = this;
          vm.selectedData = []
          vm.data = [{
